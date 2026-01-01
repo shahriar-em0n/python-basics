85821df7e05c5d6a5163c839e4e1985cbb4d7eb1
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-01 19:45:43 +0600

    how store a string in conputer

---
+++ 003_String/001_string.py
@@ -0,0 +1,14 @@
+# String is a sequence of characters within quotes
+
+str1 = "Shahriar"
+
+#  s   h   a   h   r   i   a   r
+# [0] [1] [2] [3] [4] [5] c[6] [7]  <--position of index
+
+print(str1)
+
+print(str1[0], str1[1], str1[2], str1[3], str1[4], str1[5], str1[6], str1[7])
+
+
+
+
