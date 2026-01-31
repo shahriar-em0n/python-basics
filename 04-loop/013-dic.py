eb8071a038bf3f65a4ac9b1174acc9cfc84ab2a8
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-31 22:16:06 +0600

    Add Loops

---
+++ 04-loop/013-dic.py
@@ -0,0 +1,13 @@
+dics = {
+    "Brand": "Ford",
+    "Model": "Mustang",
+    "Year": "1964",
+    "Year": "2020"
+}
+
+for key, value in enumerate(dics): # For index
+    print(key, value) 
+
+for key, value in dics.items(): # For full dic
+    print(key, value)
+    
