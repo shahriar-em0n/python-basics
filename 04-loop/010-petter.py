b4f5cdf7df0c2577010ec55a8cf98296e2c67656
Shahriar-em0n <contactshahriar12@gmail.com>
2026-01-31 22:15:17 +0600

    Add Loops

---
+++ 04-loop/010-petter.py
@@ -0,0 +1,18 @@
+# #end
+# print('*')
+# print('*', end='')
+
+# #Separator
+
+# print(1, 2, 3, 4, 5)
+# print(1, 2, 3, 4, 5, sep='  ')
+
+i = 1
+while i <= 5:
+    j = 1
+    while j <= i:
+        print('*', end='')
+        j+=1
+    print()
+    i +=1
+     
