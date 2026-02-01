1965848d922f20ec220dc3e0d819ac330ad4d425
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-01 22:02:53 +0600

    Add list program

---
+++ 007-list/001.py
@@ -0,0 +1,14 @@
+tea_varites = ['Black', 'Masala', 'White']
+print(tea_varites)
+
+
+tea_varites.insert(1, "Green")
+print(tea_varites)
+
+
+squared = [x ** 2 for x in range(10)]
+print(squared)
+
+cube_num = [y ** 3 for y in range(5)]
+
+print(cube_num)
