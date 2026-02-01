7d6aad29eef472e96f2a7b3e9f792e7788d33f83
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-01 22:03:37 +0600

    Add list program

---
+++ 007-list/004.py
@@ -0,0 +1,12 @@
+# SquaresComp
+
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in range(11, 101):
+#     list1.append(i )
+# print(list1)
+
+# list2 = [i for i in range(1, 101)]
+# print(list2)
+
+list3 = [x**3 for x in range(1, 11, 2)]
+print(list3)
