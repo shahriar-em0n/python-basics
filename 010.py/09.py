9dff35c8a25e10f7a5a95d1bbdcda1f2263b955a
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-03 15:42:25 +0600

    Add program

---
+++ 010.py/09.py
@@ -0,0 +1,10 @@
+lucky = int(input("Enter the number: "))
+
+if lucky == 6 or lucky == 7 or lucky == 9:
+    print("Lucky")
+else:
+    print("unlucky")
+    
+a  = 0
+if a in [6, 7, 9]:
+    print("Lucky")
