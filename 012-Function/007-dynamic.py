375ceabe063d92895b3cb58c1ec6027ece3d423d
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-05 12:06:35 +0600

    Add function

---
+++ 012-Function/007-dynamic.py
@@ -0,0 +1,7 @@
+def intro(name, country):
+    print("Hello Everyone!")
+    print(f"My Name is {name}")
+    print(f"I'm living {country}")
+    print('Good Bye!')
+
+intro('Mohammad Shahriar', 'Bangladesh')
