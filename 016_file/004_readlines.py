1a2e2f6d62be8373671247feebc781244064c0c7
Shahriar-em0n <contactshahriar12@gmail.com>
2026-02-09 13:02:04 +0600

    Add co de

---
+++ 016_file/004_readlines.py
@@ -0,0 +1,9 @@
+with open('016_file/readme.md', 'r') as fileOne:
+    lines = fileOne.readlines()
+    # print(lines[0])
+    # print(lines[1])
+    # print(lines[2])
+    # print(lines[3])
+    # print(lines[4])
+    print(lines)
+    print(f'lines - {len(lines)}')
